8b180547c37bb672e264af498e5c38e1d6fe35cb
gloss-water <gloss.water@gmail.com>
2017-12-17 14:53:02 -0700

    fixing webpack dev config

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import logo from '../static/witch-cafe-logo.png';
 
 const App = () => (
-	<p>
-		clean clean beginnings for a small cafe
-	</p>
+	<div id='app'>
+		<img src={logo} style={{ align: 'center' }} />
+		<p>clean clean beginnings for a small cafe</p>
+	</div>
 );
 
 export default App;
